8fe1cb8470cf852a6b44df0b11c078396467150f
jiangcheng.wxd <jiangcheng.wxd@taobao.com>
2013-08-02 11:47:14 +0800

    adc -tmd

--- adc-tmd/server/app/mix_semi/mix_semi.js
+++ adc-tmd/server/app/mix_semi/mix_semi.js
@@ -1,70 +1,35 @@
 var express = require("express"),
     app = express(),
     server = require('http').createServer(app),
-    io = require('./node_modules/socket.io/').listen(server)
+    io = require('../../node_modules/socket.io').listen(server),
+    MSG_TYPE = require('./msg')
     ;
 
+require('../../module/util/util');
+
 server.listen(8181);
+
 
 io.configure(function () {
     io.set('log level', 0);
     io.set('transports', ['websocket','xhr-polling','jsonp-polling','htmlfile']);
     /*
-    io.set('transports', ['websocket',
-                            'xhr-polling',
-                            'htmlfile',
-                            'jsonp-polling',
-                            'flashsocket']);
-    */
+     io.set('transports', ['websocket',
+     'xhr-polling',
+     'htmlfile',
+     'jsonp-polling',
+     'flashsocket']);
+     */
 });
 
 app.get('/', function (req, res) {
 
 });
 
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
-        }
-    }
-    return false;
-};
-Array.prototype.remByVal = function(val) {
-    for (var i = 0; i < this.length; i++) {
-        if (this[i] === val) {
-            this.splice(i, 1);
-            i--;
-        }
-    }
-    return this;
-};
+
 
 var pcSocket,
-    MSG_TYPE = {
-        M_CONNECT_REQ:'M_CONNECT_REQ',
-        M_CONNECT_RES:'M_CONNECT_RES',
-        M_TOKEN_INVALID:'M_TOKEN_INVALID',
-        M_TOKEN_OK:'M_TOKEN_OK',
-        M_SHAKE_INIT:'M_SHAKE_INIT',     //PC通知M端开始摇晃.
-        M_SHAKE_REQ:'M_SHAKE_REQ',
-        M_SHAKE_RES:'M_SHAKE_RES',
-        M_REPLAY:'M_REPLAY',
-
-        PC_CONNECT_REQ:'PC_CONNECT_REQ',  //PC连接请求.
-        PC_M_CONNECT_RES:'PC_M_CONNECT_RES',         //M端接入.
-        PC_M_ALL_CONNECT_RES:'PC_M_ALL_CONNECT_RES',         //所有 M端接入.
-
-        PC_M_SHAKE_START_TIP:'PC_M_SHAKE_START_TIP',   //通知所有M端开始摇晃.
-
-        PC_SHAKE_RES:'PC_SHAKE_RES',
-
-        PC_REPLAY:'PC_REPLAY'  //重玩
-    }
-    ;
-
-var MSocket = [],
+    MSocket = [],
     TokenArr = []
     ;
 
@@ -136,14 +101,4 @@ io.sockets.on('connection', function (socket) {
         }
     });
 });
-
-//for(var i = 0; i < MSocket.length; i++){
-//
-//    MSocket[i].on(MSG_TYPE.M_SHAKE_REQ, function (data) {
-//
-//        if(pcSocket){
-//            pcSocket.emit(MSG_TYPE.PC_SHAKE_RES, data);
-//        }
-//    });
-//}
 
